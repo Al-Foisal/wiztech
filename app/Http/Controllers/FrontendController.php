d2b3e2ebcb0034d9559c02e7dd198dea1a82d352
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2022-11-10 10:06:38 +0600

    forgot: time limit added

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use App\Models\Doctor;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -20,23 +21,32 @@ class FrontendController extends Controller {
     }
 
     public function doctorBooking(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'reason' => 'required',
-            'phone'  => 'required',
-        ]);
+        $now       = new Datetime("now");
+        $begintime = new DateTime('10:30');
+        $endtime   = new DateTime('17:00');
 
-        if ($validator->fails()) {
-            return back()->with('toast_error', $validator->messages()->all())->withInput();
+        if ($now >= $begintime && $now <= $endtime) {
+            $validator = Validator::make($request->all(), [
+                'reason' => 'required',
+                'phone'  => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return back()->with('toast_error', $validator->messages()->all())->withInput();
+            }
+
+            Booking::create([
+                'doctor_id' => $request->doctor_id,
+                'user_id'   => Auth::id(),
+                'reason'    => $request->reason,
+                'phone'     => $request->phone,
+            ]);
+
+            return redirect()->route('doctorList')->withToastSuccess('Your booking successful!');
+        } else {
+            return redirect()->route('doctorList')->withToastError('Your booking within 10 am to 5pm!');
         }
 
-        Booking::create([
-            'doctor_id' => $request->doctor_id,
-            'user_id'   => Auth::id(),
-            'reason'    => $request->reason,
-            'phone'     => $request->phone,
-        ]);
-
-        return redirect()->route('doctorList')->withToastSuccess('Your booking successful!');
     }
 
     public function dashboard() {
